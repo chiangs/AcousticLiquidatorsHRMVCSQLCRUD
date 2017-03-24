2001885e0694cd04aaea9349af15bab4ba69936e
Developer <stephen.e.chiang@gmail.com>
2017-03-23 22:26:30 -0600

    Updated README and created DAO/Impl shell

--- src/main/java/com/sd/data/HRDAO.java
+++ src/main/java/com/sd/data/HRDAO.java
@@ -1,5 +1,12 @@
 package com.sd.data;
 
-public interface HRDAO {
+import java.util.List;
 
-}
+public interface HRDAO {
+	 	Employee getEmployeeById(Employee emp);
+//	 	Employee getEmployeeByNameContains(Employee emp);
+	 	Employee addEmployee(Employee emp);
+		Employee updateEmployee(Employee emp);
+		void deactivateEmployee(Employee emp);
+		List<Employee> listEmployees();
+	}
--- src/main/java/com/sd/data/HRDAOImpl.java
+++ src/main/java/com/sd/data/HRDAOImpl.java
@@ -1,5 +1,37 @@
 package com.sd.data;
 
+import java.util.List;
+
 public class HRDAOImpl implements HRDAO {
+
+	@Override
+	public Employee getEmployeeById(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee addEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee updateEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deactivateEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Employee> listEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
